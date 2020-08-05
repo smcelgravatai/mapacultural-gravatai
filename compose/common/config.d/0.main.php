fecc6c2d073918c983741015b3d82895f3742292
jordan.olsefer <master@olsefer.com>
2020-08-05 13:17:22 -0300

    alteração credenciais db

--- compose/common/config.d/0.main.php
+++ compose/common/config.d/0.main.php
@@ -18,8 +18,8 @@ return [
     'doctrine.database' => [
         'host'           => env('DB_HOST', 'db'),
         'dbname'         => env('DB_NAME', 'mapas'),
-        'user'           => env('DB_USER', 'mapas'),
-        'password'       => env('DB_PASS', 'mapas'),
+        'user'           => env('DB_USER', 'mapas_gravatai'),
+        'password'       => env('DB_PASS', 'smcelm@p@5'),
         'server_version' => env('DB_VERSION', 10),
     ]
 ];
